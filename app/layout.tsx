f48816711f96919c2a23d3eac145c38dbe85bc25
Nikita <nikistosov@gmail.com>
2024-10-28 13:12:29 +0100

    feat: bootstrap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "./globalStyles/globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
