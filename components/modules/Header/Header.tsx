608b1133b6480c9c6d0571aeb0d75b668f39af9d
Nikita <nikistosov@gmail.com>
2024-10-28 16:48:40 +0100

    feat: icons

--- components/modules/Header/Header.tsx
+++ components/modules/Header/Header.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useLang } from "@/hooks/useLang";
+import Logo from "@/components/elements/Logo/Logo";
 
 const Header = () => {
   const { lang, translations } = useLang();
@@ -10,6 +11,10 @@ const Header = () => {
         <button className="btn-reset header__burger">
           {translations[lang].header.menu_btn}
         </button>
+        <div className="header__logo">
+          <Logo />
+        </div>
+        <ul className="header__links"></ul>
       </div>
     </header>
   );
