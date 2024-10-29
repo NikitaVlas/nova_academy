b90625dcb4b1945de455779f998f3840492ee32d
Nikita <nikistosov@gmail.com>
2024-10-29 13:15:28 +0100

    feat: languege translater

--- components/modules/Header/Menu.tsx
+++ components/modules/Header/Menu.tsx
@@ -5,14 +5,23 @@ import { $menuIsOpen, closeMenu } from "@/context/modals";
 import Logo from "@/components/elements/Logo/Logo";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { removeOverflowHiddenFromBody } from "@/lib/utils/common";
+import {AllowedLangs} from "@/constants/lang";
+import {setLang} from "@/context/lang";
 
 const Menu = () => {
   const [showBuyerList, setShowBuyerList] = useState(false);
   const [showCOntactList, setShowContactList] = useState(false);
   const menuIsOpen = useUnit($menuIsOpen);
   const { lang, translations } = useLang();
-  const isMedia800 = useMediaQuery(800);
-  const isMedia640 = useMediaQuery(640);
+
+  const handelSwitchLang = (lang: string) => {
+    setLang(lang as AllowedLangs)
+    localStorage.setItem("lang", JSON.stringify(lang));
+  }
+
+  const handleSwitchLangToRu = () => handelSwitchLang('ru')
+  const handleSwitchLangToEn = () => handelSwitchLang('en')
+  const handleSwitchLangToDe = () => handelSwitchLang('de')
 
   const handelCloseMenu = () => {
     removeOverflowHiddenFromBody()
@@ -21,18 +30,41 @@ const Menu = () => {
 
   return (
     <nav className={`nav-menu ${menuIsOpen ? "open" : "close"}`}>
-      <div className={`nav-menu__logo ${menuIsOpen ? "open" : ""}`}>
-        <Logo />
+      <div className="container">
+        <div className={`nav-menu__logo ${menuIsOpen ? "open" : ""}`}>
+          <Logo />
+        </div>
+        <button
+          className={`btn-reset nav-menu__close ${menuIsOpen ? "open" : ""}`}
+          onClick={handelCloseMenu}
+        />
+        <div className={`nav-menu__lang ${menuIsOpen ? "open" : ""}`}>
+          <button
+            className={`btn-reset nav-menu__lang__btn ${
+              lang === "de" ? "lang-active" : ""
+            }`}
+            onClick={handleSwitchLangToDe}
+          >
+            DE
+          </button>
+          <button
+            className={`btn-reset nav-menu__lang__btn ${
+              lang === "en" ? "lang-active" : ""
+            }`}
+            onClick={handleSwitchLangToEn}
+          >
+            EN
+          </button>
+          <button
+            className={`btn-reset nav-menu__lang__btn ${
+              lang === "ru" ? "lang-active" : ""
+            }`}
+            onClick={handleSwitchLangToRu}
+          >
+            RU
+          </button>
+        </div>
       </div>
-      {/*<img*/}
-      {/*  className={`nav-menu__bg ${menuIsOpen ? "open" : ""}`}*/}
-      {/*  src={`/img/menu-bg${isMedia800 ? "-small" : ""}.png`}*/}
-      {/*  alt="menu background"*/}
-      {/*/>*/}
-      <button
-        className={`btn-reset nav-menu__close ${menuIsOpen ? "open" : ""}`}
-        onClick={handelCloseMenu}
-      />
     </nav>
   );
 }
