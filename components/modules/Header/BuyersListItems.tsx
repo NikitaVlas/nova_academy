a7dc2bef461478c72bccd19658b34ca31e03e288
Nikita <nikistosov@gmail.com>
2024-10-30 12:30:41 +0100

    feat: bayerList component

---
+++ components/modules/Header/BuyersListItems.tsx
@@ -0,0 +1,17 @@
+
+import Link from 'next/link'
+import { useLang } from '@/hooks/useLang'
+
+const BuyersListItems = () => {
+  const { lang, translations } = useLang()
+
+  return (
+    <>
+      <li className='nav-menu__accordion__item'>
+
+      </li>
+    </>
+  )
+}
+
+export default BuyersListItems
