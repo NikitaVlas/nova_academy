cad1c58822810f727708ed7746b10f1d25c15574
Nikita <nikistosov@gmail.com>
2024-10-30 12:30:57 +0100

    feat: contactsList component

---
+++ components/modules/Header/ContactsListItems.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+import { useLang } from '@/hooks/useLang'
+
+const ContactsListItems = () => {
+  const { lang, translations } = useLang()
+
+  return (
+    <>
+      <li className='nav-menu__accordion__item'>
+
+      </li>
+    </>
+  )
+}
+
+export default ContactsListItems
