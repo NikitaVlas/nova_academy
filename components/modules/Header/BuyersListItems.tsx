df765b8bfcd51866ed447cf9d878b2d8baa9e598
Nikita <nikistosov@gmail.com>
2024-10-30 13:08:47 +0100

    feat: buyerList update

--- components/modules/Header/BuyersListItems.tsx
+++ components/modules/Header/BuyersListItems.tsx
@@ -1,14 +1,35 @@
 
 import Link from 'next/link'
-import { useLang } from '@/hooks/useLang'
+import { useLang } from "@/hooks/useLang";
 
 const BuyersListItems = () => {
   const { lang, translations } = useLang()
 
   return (
     <>
-      <li className='nav-menu__accordion__item'>
-
+      <li className="nav-menu__accordion__item">
+        <Link
+          href="/course"
+          className="nav-menu__accordion__item__link nav-menu__accordion__item__title"
+        >
+          {translations[lang].main_menu.course}
+        </Link>
+      </li>
+      <li className="nav-menu__accordion__item">
+        <Link
+          href="/trainers"
+          className="nav-menu__accordion__item__link"
+        >
+          {translations[lang].main_menu.trainers}
+        </Link>
+      </li>
+      <li className="nav-menu__accordion__item">
+        <Link
+          href="/prices"
+          className="nav-menu__accordion__item__link"
+        >
+          {translations[lang].main_menu.prices}
+        </Link>
       </li>
     </>
   )
